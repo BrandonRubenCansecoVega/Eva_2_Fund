6c903e6a4f58ac2c808bd27cd5530c8dfdb84e8d
Brandon Canseco <159494671+BrandonRubenCansecoVega@users.noreply.github.com>
2024-03-30 16:01:50 -0700

    Add files via upload

---
+++ EVA2_12_SERVICIO_SOCIAL/src/eva2_12_servicio_social/EVA2_12_SERVICIO_SOCIAL.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_12_servicio_social;
+
+import static java.time.Clock.system;
+import java.util.Scanner;
+
+/**
+ *
+ * @author rubra
+ */
+public class EVA2_12_SERVICIO_SOCIAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int creditos, semestre;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Que semestre cursan?");
+        semestre = captu.nextInt();
+        
+        
+        
+                }
+    
+}
